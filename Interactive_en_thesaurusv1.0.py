feb5e7ee3144092b40b865122b7b0b7489854102
Nikolay Mironov <nikolai.mironov.1@gmail.com>
2021-09-03 15:20:42 +0300

    Create Interactive_en_thesaurusv1.0.py

---
+++ Interactive_en_thesaurusv1.0.py
@@ -0,0 +1,30 @@
+import json
+from difflib import get_close_matches
+data = json.load(open("Resources/data.json"))
+
+def definitions(word):
+    word = word.lower()
+    if word in data:
+        return data[word]
+    elif len(get_close_matches(word, data.keys(), cutoff=0.8)) > 0:
+        confirm = input("Did you mean '%s' instead? Enter Y if yes or N if no: " % get_close_matches(word, data.keys())[0])
+        if confirm == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif confirm == "N":
+            return "PLease, check the word you are searching for."
+        else:
+            return "Incorrect input!"
+    else:
+        return "The word doesn't exists"
+
+
+word = input("Enter a word: ")
+output = definitions(word)
+
+#print(" ".join(definitions(word)))
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
